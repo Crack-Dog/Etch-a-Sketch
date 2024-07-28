f0b6968402cc54147382613e8356d4aa67c88ec3
Wesley Mak <wesleymak005@gmail.com>
2024-07-28 15:39:23 -0400

    added 256 divs with js

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-console.log("Hello")
+console.log("Hello");
+
+for (let i = 0; i <16; i++){
+    for (let j = 0; j <16; j++){
+    const sect = document.querySelector(".gridcontainer");
+    const sqdivs = document.createElement("p");
+    sqdivs.textContent = "Test";
+    sect.appendChild(sqdivs);
+    }
+}
