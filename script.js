2f31b1e26e10fc2b43741d441ef5c032f9d6c3c2
Wesley Mak <wesleymak005@gmail.com>
2024-07-31 13:10:30 -0400

    Add change colour on mousehover to grid

--- script.js
+++ script.js
@@ -3,13 +3,23 @@ console.log("Hello");
 for (let i = 0; i <16; i++){
     for (let j = 0; j <16; j++){
     const sect = document.querySelector(".gridcontainer");
-    const sqdivs = document.createElement("p");
+    const sqdivs = document.createElement("div");
+    sqdivs.classList.add('gridsq');
     sqdivs.textContent = "Test";
     sect.appendChild(sqdivs);
     }
 }
 
-const hoverevent = document.querySelector("p");
-hoverevent.addEventListener("mouseover", () => {
-    hoverevent.style.backgroundColor = "red";
-});
+document.body.addEventListener("mouseover", event =>{
+    if(event.target.classList.contains("gridsq")){
+        event.target.style.background = "black";
+        console.log("test");
+    }
+})
+
+document.body.addEventListener("mouseout", event =>{
+    if(event.target.classList.contains("gridsq")){
+        event.target.style.background = "blue";
+        console.log("test");
+    }
+})
