ac34efeef3c728817631aa6349afee4d6c3f5da8
Wesley Mak <wesleymak005@gmail.com>
2024-07-31 13:53:03 -0400

    add createGrid  function + button, need to fix css

--- script.js
+++ script.js
@@ -1,5 +1,32 @@
 console.log("Hello");
 
+var gridLength = 0
+createGrid(gridLength)
+
+function createGrid(gridSize){
+    if (gridLength = 0){
+        for (let i = 0; i <16; i++){
+            for (let j = 0; j <16; j++){
+            const sect = document.querySelector(".gridcontainer");
+            const sqdivs = document.createElement("div");
+            sqdivs.classList.add('gridsq');
+            sqdivs.textContent = "Test";
+            sect.appendChild(sqdivs);
+            }
+        }
+    }
+    else{
+        for (let i = 0; i < gridSize; i++){
+            for (let j = 0; j < gridSize; j++){
+            const sect = document.querySelector(".gridcontainer");
+            const sqdivs = document.createElement("div");
+            sqdivs.classList.add('gridsq');
+            sqdivs.textContent = "Test";
+            sect.appendChild(sqdivs);
+            }
+        }
+    }
+}
 for (let i = 0; i <16; i++){
     for (let j = 0; j <16; j++){
     const sect = document.querySelector(".gridcontainer");
@@ -22,4 +49,16 @@ document.body.addEventListener("mouseout", event =>{
         event.target.style.background = "blue";
         console.log("test");
     }
+})
+
+var gridLength = 0
+
+const btn = document.querySelector("#btn")
+btn.addEventListener("click", () => {
+    gridLength = prompt("Enter grid length (ex: 16 for 16x16");
+    if (gridLength > 100){
+        gridLength = 100
+    }
+    document.querySelectorAll(".gridsq").forEach((event) => event.parentNode.removeChild(event));
+    createGrid(gridLength);
 })
